6e66703136d2a32fe32e94391ce05ec45632eab8
moises <moisesestevao2004@hotmail.com>
2022-11-20 20:49:34 -0300

    initial commit

---
+++ inventory/src/main/kotlin/com/openshop/inventory/InventoryApplication.kt
@@ -0,0 +1,11 @@
+package com.openshop.inventory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventoryApplication
+
+fun main(args: Array<String>) {
+	runApplication<InventoryApplication>(*args)
+}
